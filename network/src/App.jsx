0ad475bc1ea08370d0c848f0aaf68963456268b0
Aaron-Thomas-Blessen <aaronthomas232200@gmail.com>
2024-10-08 18:16:17 +0530

    signin user seller  basic added

--- network/src/App.jsx
+++ network/src/App.jsx
@@ -1,11 +1,17 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Home from "./pages/home";
+import Home from "./pages/Home";
+import SignUp from "./pages/SignUp";
+import UserSignUp from "./pages/User/UserSignUp";
+import SellerSignUp from "./pages/Seller/SellerSignUp";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/user-signup" element={<UserSignUp />} />
+        <Route path="/seller-signup" element={<SellerSignUp />} />
       </Routes>
     </Router>
   );
