37d94494aa486d1e933c40b8661d87e5aee49d9b
Aaron Thomas Blessen <aaronthomas232200@gmail.com>
2025-01-17 01:50:20 +0530

    final ui fixes

--- src/pages/Seller/SellerAuth.jsx
+++ src/pages/Seller/SellerAuth.jsx
@@ -38,7 +38,7 @@ export default function SellerAuth() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
-        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+        <h2 className="mt-6 text-center text-3xl font-bold text-gray-900">
           {isLogin ? "Sign in as a Seller" : "Create a Seller account"}
         </h2>
         <p className="mt-2 text-center text-sm text-gray-600">
